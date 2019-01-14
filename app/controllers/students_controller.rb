c5698b32a0206fcbe05e4ff3be0669cad91e6336
github username <github email address>
2019-01-14 02:00:27 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,33 @@
 class StudentsController < ApplicationController
-  
-  def index
-    @students = Student.all
-  end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+    def index
+        @students = Student.all
+    end
+
+    def show
+        @student = Student.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        # Never do below....long way of doing it.
+        @student = Student.new({first_name: params[:first_name], last_name: params[:last_name]})
+        @student.save
+        redirect_to @student
+    end
 
 end
+
+# Prefix Verb        URI Pattern                              Controller#Action
+
+# welcome_index      GET    /welcome/index(.:format)          welcome#index
+# articles           GET    /articles(.:format)               articles#index
+#                    POST   /articles(.:format)               articles#create
+# new_article        GET    /articles/new(.:format)           articles#new
+# edit_article       GET    /articles/:id/edit(.:format)      articles#edit
+# article            GET    /articles/:id(.:format)           articles#show
+#                    PATCH  /articles/:id(.:format)           articles#update
+#                    PUT    /articles/:id(.:format)           articles#update
+#                    DELETE /articles/:id(.:format)           articles#destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
   resources :students, only: :index
-  get '/students/:id', to: 'students#show', as: 'student'
+  get '/student/:id', to: 'students#show', as: 'student'
+  post 'students', to: 'students#create', as: 'create_student'
+  get 'students/new', to: 'students#new', as: 'new_student'
 end
+# Prefix Verb        URI Pattern                              Controller#Action
+
+# welcome_index      GET    /welcome/index(.:format)          welcome#index
+# articles           GET    /articles(.:format)               articles#index
+#                    POST   /articles(.:format)               articles#create
+# new_article        GET    /articles/new(.:format)           articles#new
+# edit_article       GET    /articles/:id/edit(.:format)      articles#edit
+# article            GET    /articles/:id(.:format)           articles#show
+#                    PATCH  /articles/:id(.:format)           articles#update
+#                    PUT    /articles/:id(.:format)           articles#update
+#                    DELETE /articles/:id(.:format)           articles#destroy
